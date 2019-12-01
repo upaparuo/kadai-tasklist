332ef33c1dcf4678b929749a957d4c4aabcc34c3
paru.upao <terahaya7991@gmail.com>
2019-12-01 16:30:22 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,31 @@
 class TasksController < ApplicationController
     
     def index
+        @tasks = Task.all
     end
     
     def show
+        @task = Task.find(params[:id])
+    end
+    
+    def new
+        @task = Task.new
     end
     
     def create
+        @task = Task.new(task_params)
+        
+        if @task.save
+            flash[:success] = 'Taskが正常に投稿されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'Taskが投稿されませんでした'
+            render :new
+        end
     end
     
     def edit
+        @task = Task.find(params[:id])
     end
     
     def update
@@ -18,4 +34,10 @@ class TasksController < ApplicationController
     def destroy
     end
     
+end
+
+private
+
+def task_params
+    params.require(:task).permit(:content)
 end
