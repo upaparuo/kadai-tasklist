0eade0f21e28aba450e3ab3199aefa25db7d5ec4
paru.upao <terahaya7991@gmail.com>
2019-12-18 13:16:47 +0000

    resubmit tasklist login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit]
     before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
     
     def index
       if logged_in?
@@ -63,6 +64,14 @@ class TasksController < ApplicationController
     
     def set_task
         @task = Task.find(params[:id])
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
+        
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
   get 'users/new'
   get 'users/create'
   get 'toppages/index'
@@ -16,6 +14,6 @@ Rails.application.routes.draw do
     
     resources :tasks
     get 'signup', to: 'users#new'
-    resources :users, only: [:index, :show, :new, :create]
+    resources :users, only: [:new, :create]
 
 end
